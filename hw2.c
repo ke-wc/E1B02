8ec2388f020cf9ffbe04e9e3e87de23e45c97d26
ke-wc <u11302202@o365.ttu.edu.tw>
2025-04-27 04:10:37 +0800

    全部

--- hw2.c
+++ hw2.c
@@ -2,10 +2,11 @@
 #include<stdlib.h>
 int main(void)
 {
-	int i=0,j=0,password=0,wrong=0;
-	char tem,cha,cha1;
+	int i=0,j=0,password=0,wrong=0,k,cha2;
+	char tem,cha,cha1,cha3;
 	//第一題倒三角 
-	/*for(i=20;i>=1;i--){
+	int trans;
+	for(i=20;i>=1;i--){
 		for(j=1;j<=i;j++){ 
 		printf("*");
 		}
@@ -13,7 +14,7 @@ int main(void)
 	}
 	scanf("%c",&tem);
 	system("cls");
-	
+	fflush(stdin);
 	
 	for(i=1;i<=3;i++){
 		printf("enter the password==>");
@@ -28,36 +29,38 @@ int main(void)
 		
 	} 
 	if(wrong==3){
-		system("pause");
 		return 0;
 	}
-	scanf("%c",&tem);
-	system("cls");*/
+	system("pause");
+	system("cls");
 	
+	while(1){
 	//第二題
-	/*printf("a. 畫出直角三角形\n");
+	printf("a. 畫出直角三角形\n");
 	printf("b. 顯示乘法表\n");
 	printf("c. 結束\n");
-	//第三題*/
+	//第三題
+	printf("enter a(A) or b(B) or c(C)==>");
+	scanf(" %c",&cha);
 	
-	/*printf("enter a(A) or b(B) or c(C)==>");
-	scanf("%c",&cha);
 	if(cha=='A'||cha=='a'){
-		scanf("%c",&tem);
-		system("cls");
 		printf("enter the char a to n==>");
-		scanf("%c",&cha1);
+		scanf(" %c",&cha1);
+			
 		for(;;){
+			if (cha1 >= 'a' && cha1 <= 'n') {
+        	break;
 			if(cha1<'a'||cha1>'n'){
-			scanf("%c",&cha1);
 			printf("reenter the char a to n==>");
-			continue;
-			}else{
-				break;
+			scanf(" %c",&cha1);
+			
+			
+		
+				}
 			}
 		}
-	}
-	int trans=cha1-'a',k;
+	system("cls");
+	trans=cha1-'a';
 	for(i=0;i<=trans;i++){
 		for(k=0;k<trans-i;k++){
 			printf(" ");	
@@ -66,10 +69,14 @@ int main(void)
 			printf("%c",'a'+j);
 		}
 		printf("\n");
-	}*/
+	}
+	system("pause");
+	system("cls");
+	
 	//第四題 
-	int cha2=0;
-	if(cha=='B'||cha=='b'){
+	
+	}
+	else if(cha=='B'||cha=='b'){
 		printf("enter the number between 0 and 9==>");
 		scanf("%d",&cha2);
 		for(;;){
@@ -78,19 +85,41 @@ int main(void)
 			}else{
 				printf("reenter the number==>");
 				scanf("%d",&cha2);
-				continue;
 			}
 		}
-	}
+	system("cls");
 	
 	for(i=1;i<=cha2;i++){
 		for(j=1;j<=cha2;j++){
-			printf("%d*%d ",i,j);
+			printf("%d*%d=%2d ",i,j,i*j);
 		}
 		printf("\n");
 	}
-	//第五題
-	printf("c");
 	system("pause");
+	system("cls");
+	//第五題
+	
+	
+	getchar();
+	}
+	else if(cha=='C'||cha=='c'){
+		printf("Continue? (y/n)");
+		scanf("%c",&cha3);
+		if(cha3=='N'||cha3=='n'){
+		break;
+		}
+		else if(cha3=='Y'||cha3=='y'){
+			system("cls");
+			continue;
+		}
+		else {
+            
+			system("pause");
+			system("cls");
+        }
+	}
+}	
+	
 	return 0;
+
 }
