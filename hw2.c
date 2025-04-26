7232cf169adb000ea579c834e86c9a17465803f6
ke-wc <u11302202@o365.ttu.edu.tw>
2025-04-26 23:13:02 +0800

    第三題不知如何用字元設定最大值

--- hw2.c
+++ hw2.c
@@ -3,7 +3,7 @@
 int main(void)
 {
 	int i,j,password=0,wrong=0;
-	char tem;
+	char tem,cha,cha1,k;
 	//第一題倒三角 
 	/*for(i=20;i>=1;i--){
 		for(j=1;j<=i;j++){ 
@@ -35,10 +35,35 @@ int main(void)
 	system("cls");*/
 	
 	//第二題
-	printf("a. 畫出直角三角形\n");
+	/*printf("a. 畫出直角三角形\n");
 	printf("b. 顯示乘法表\n");
 	printf("c. 結束\n");
+	//第三題*/
 	
+	printf("enter a(A) or b(B) or c(C)==>");
+	scanf("%c",&cha);
+	if(cha=='A'||cha=='a'){
+		scanf("%c",&tem);
+		system("cls");
+		printf("enter the char a to n==>");
+		scanf("%c",&cha1);
+		for(;;){
+			if(cha1<'a'||cha1>'n'){
+			scanf("%c",&cha1);
+			printf("reenter the char a to n==>");
+			continue;
+			}else{
+				break;
+			}
+		}
+	}
+	for(k='a';k<=cha;k++){
+		printf("1");
+	}
+	
+	
+	
+	 
 	system("pause");
 	return 0;
 }
