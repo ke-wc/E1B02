151c8628c74231408e66650b603055cc87360307
ke-wc <u11302202@o365.ttu.edu.tw>
2025-04-26 22:13:21 +0800

    第一題wrong執行三次沒有程式結束

---
+++ hw2.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(void)
+{
+	int i,j,password=0,wrong=0;
+	char tem;
+	//第一題倒三角 
+	/*for(i=20;i>=1;i--){
+		for(j=1;j<=i;j++){ 
+		printf("*");
+		}
+		printf("\n"); 
+	}
+	scanf("%c",&tem);
+	system("cls");*/
+	
+	
+	for(i=1;i<=3;i++){
+		printf("enter the password==>");
+		scanf("%d",&password);
+		if(password==2025){
+			printf("correct\n");
+			break;
+			}else{
+				printf("wrong\n");
+				wrong++;		
+		}
+		
+	} 
+	if(wrong==3){
+		system("pause");
+		return 0;
+	}
+	printf("123");
+	system("pause");
+	return 0;
+}
