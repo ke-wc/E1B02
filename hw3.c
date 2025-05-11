812a6e7a51d3faba15761451c259dc8d28a6729b
ke-wc <u11302202@o365.ttu.edu.tw>
2025-05-11 15:04:03 +0800

    第二題A 有時侯的*不會有10個

--- hw3.c
+++ hw3.c
@@ -1,12 +1,16 @@
 #include<stdio.h>
 #include<stdlib.h>
-int main(void)
+#include<windows.h> 
+#include <time.h>
+int main(void
 {
-	int i=0,j=0,password=0,wrong=0,k,cha2;
-	char tem,cha,cha1,cha3;
+	int i=0,j=0,password=0,wrong=0;
+	char cha;
+	char seats[9][9];
+
 	//第一題倒三角 
-	int trans;
-	for(i=20;i>=1;i--){
+
+	/*for(i=20;i>=1;i--){
 		for(j=1;j<=i;j++){ 
 		printf("*");
 		}
@@ -31,7 +35,36 @@ int main(void)
 		return 0;
 	}
 	system("pause");
-	system("cls");
+	system("cls");*/
+	printf("a. Available seats\n");
+	printf("b. Arrange for you\n");
+	printf("c. Choose by yourself\n");
+	printf("d. Exit\n");
+	
+	printf("輸入==>");
+	scanf("%c",&cha);
+	
+	if(cha=='a'||cha=='A'){
+		for(i=0;i<9;i++){
+		for(j=0;j<9;j++){
+			seats[i][j]='_';
+		}
+	}
+		srand(time(NULL));
+		for (i = 0; i < 10; i++) {
+        int r = rand() % 9;  
+        int c = rand() % 9;  
+        seats[r][c] = '*';    
+    }
+		printf("\\123456789\n");
+		for(i = 0; i < 9; i++) {
+        printf("%d", 9 - i);  
+        for(j = 0; j < 9; j++) {
+            printf("%c", seats[i][j]);  
+        }
+        printf("\n");  
+    }
+}
 
 	system("pause");
 	return 0;	
