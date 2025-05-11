eefe186123ad8dc52153436aa650b56c365b7753
ke-wc <u11302202@o365.ttu.edu.tw>
2025-05-11 14:10:43 +0800

    第一題倒三角

---
+++ hw3.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(void)
+{
+	int i=0,j=0,password=0,wrong=0,k,cha2;
+	char tem,cha,cha1,cha3;
+	//第一題倒三角 
+	int trans;
+	for(i=20;i>=1;i--){
+		for(j=1;j<=i;j++){ 
+		printf("*");
+		}
+		printf("\n"); 
+	}
+	system("pause");
+	system("cls");
+
+
+
+	system("pause");
+	return 0;	
+}
